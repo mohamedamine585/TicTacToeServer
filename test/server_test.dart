d1a152643d1026ea1a5db1cf29d957938262aa2e
Mohamed amine Tlili <foxweb585@gmail.com.com>
2023-10-20 13:14:23 +0100

    5th

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,20 +1,17 @@
-import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
-import 'package:http/http.dart';
 import 'package:test/test.dart';
+
+import 'test_case.dart';
 
 void main() {
   final port1 = '8080';
   final host1 = 'ws://127.0.0.1:$port1';
   final port2 = '8081';
   final host2 = 'http://127.0.0.1:$port2';
-  final playername0 = 'medaminetlili11';
-  final playerpassword0 = playername0;
-  final playername1 = 'medaminetlili12';
-  final playerpassword1 = playername0;
   late Process p;
-  var message0 = "Room created";
+  var message0;
 
   setUp(() async {
     p = await Process.start(
@@ -25,74 +22,29 @@ void main() {
 
     // Wait for server to start and print to stdout.
   });
+  List<String> playernames = generateNames(8);
+  List<String> passwords = generatePasswords(8);
+  test_case(playernames, passwords, host1, host2);
 
-  group('********** Test case **************', () {
-    group(' **************    Player 0    *************** ', () {
-      var token0;
+  tearDownAll(() => p.kill());
+}
 
-      test('Signup', () async {
-        var response = await get(Uri.parse(
-            '$host2/Signup/?playername=$playername0&password=$playerpassword0'));
-        expect(jsonDecode(response.body)["message"], "Player is signed up");
-      });
+List<String> generateNames(int count) {
+  final List<String> names = [];
+  for (int i = 0; i < count; i++) {
+    names.add('UserAlphaa${i + 1}');
+  }
+  return names;
+}
 
-      test('Signin', () async {
-        var response = await get(Uri.parse(
-            '$host2/Signin/?playername=$playername0&password=$playerpassword0'));
-        token0 = jsonDecode(response.body)["token"];
-        expect(jsonDecode(response.body)["message"], "Player is signed in");
-      });
-
-      test('Ask To Play 0', () async {
-        WebSocket player0 =
-            await WebSocket.connect(host1, headers: {"token": token0});
-        player0.listen((event) {
-          expect(jsonDecode(event)["message"], message0);
-        }, onDone: () {
-          player0.close();
-        });
-      });
-    });
-    group(' **************   Player 1   *************** ', () {
-      var token1;
-      test('Signup', () async {
-        var response = await get(Uri.parse(
-            '$host2/Signup/?playername=$playername1&password=$playerpassword1'));
-        expect(jsonDecode(response.body)["message"], "Player is signed up");
-      });
-
-      test('Signin', () async {
-        var response = await get(Uri.parse(
-            '$host2/Signin/?playername=$playername1&password=$playerpassword1'));
-        token1 = jsonDecode(response.body)["token"];
-        expect(jsonDecode(response.body)["message"], "Player is signed in");
-      });
-
-      test('Ask To Play 1', () async {
-        WebSocket player1 =
-            await WebSocket.connect(host1, headers: {"token": token1});
-        message0 = "Opponent found !";
-        player1.listen((event) {
-          expect(jsonDecode(event)["message"], "Opponent found !");
-        }, onDone: () {
-          player1.close();
-        });
-      });
-
-      test('Delete', () async {
-        var response = await get(Uri.parse(
-            '$host2/Delete/?playername=$playername0&password=$playerpassword0'));
-        expect(jsonDecode(response.body)["res"], true);
-        expect(jsonDecode(response.body)["res"], true);
-      });
-      test('Delete', () async {
-        var response = await get(Uri.parse(
-            '$host2/Delete/?playername=$playername1&password=$playerpassword1'));
-        expect(jsonDecode(response.body)["res"], true);
-        expect(jsonDecode(response.body)["res"], true);
-      });
-    });
-
-    tearDownAll(() => p.kill());
-  });
+List<String> generatePasswords(int count) {
+  final List<String> passwords = [];
+  final Random random = Random();
+  for (int i = 0; i < count; i++) {
+    // Generate a random 8-character password
+    final password =
+        String.fromCharCodes(List.generate(8, (_) => random.nextInt(26) + 97));
+    passwords.add(password);
+  }
+  return passwords;
 }
---
+++ test/test_case.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart';
+
+import 'package:test/test.dart';
+
+test_case(List<String> playernames, List<String> passwords, String host1,
+    String host2) {
+  var message0;
+  group('************** Test ***************', () {
+    group('**********  Test Create***************', () {
+      for (int i = 0; i < 3; i++) {
+        group('********** Test case ${i / 2} **************', () {
+          group(' **************    Player ${i / 2} 0    *************** ', () {
+            var token0;
+            message0 = "Room created";
+
+            test('Signup', () async {
+              var response = await get(Uri.parse(
+                  '$host2/Signup/?playername=${playernames[i]}&password=${passwords[i]}'));
+              expect(
+                  jsonDecode(response.body)["message"], "Player is signed up");
+            });
+
+            test('Signin', () async {
+              var response = await get(Uri.parse(
+                  '$host2/Signin/?playername=${playernames[i]}&password=${passwords[i]}'));
+              token0 = jsonDecode(response.body)["token"];
+              expect(
+                  jsonDecode(response.body)["message"], "Player is signed in");
+            });
+
+            test('Ask To Play 0', () async {
+              WebSocket player0 =
+                  await WebSocket.connect(host1, headers: {"token": token0});
+              player0.listen((event) {
+                expect(jsonDecode(event)["message"], message0);
+              }, onDone: () {
+                player0.close();
+              });
+            });
+          });
+          group(' **************   Player $i 1   *************** ', () {
+            var token1;
+            test('Signup', () async {
+              var response = await get(Uri.parse(
+                  '$host2/Signup/?playername=${playernames[i + 1]}&password=${passwords[i + 1]}'));
+              expect(
+                  jsonDecode(response.body)["message"], "Player is signed up");
+            });
+
+            test('Signin', () async {
+              var response = await get(Uri.parse(
+                  '$host2/Signin/?playername=${playernames[i + 1]}&password=${passwords[i + 1]}'));
+              token1 = jsonDecode(response.body)["token"];
+              expect(
+                  jsonDecode(response.body)["message"], "Player is signed in");
+            });
+
+            test('Ask To Play 1', () async {
+              WebSocket player1 =
+                  await WebSocket.connect(host1, headers: {"token": token1});
+              message0 = "Opponent found !";
+              player1.listen((event) {
+                expect(jsonDecode(event)["message"], "Opponent found !");
+              }, onDone: () {
+                player1.close();
+              });
+            });
+          });
+        });
+        i++;
+      }
+    });
+
+    group('******* Test Delete  *******', () {
+      for (int i = 0; i < 3; i++) {
+        test('Delete  ${i / 2} 0 ', () async {
+          var response = await get(Uri.parse(
+              '$host2/Delete/?playername=${playernames[i]}&password=${passwords[i]}'));
+          expect(jsonDecode(response.body)["res"], true);
+          expect(jsonDecode(response.body)["res"], true);
+        });
+        test('Delete ${i / 2} 1', () async {
+          var response = await get(Uri.parse(
+              '$host2/Delete/?playername=${playernames[i + 1]}&password=${passwords[i + 1]}'));
+          expect(jsonDecode(response.body)["res"], true);
+          expect(jsonDecode(response.body)["res"], true);
+        });
+        i++;
+      }
+    });
+  });
+}
