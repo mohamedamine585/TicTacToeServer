741c2eefc25d164233bf67b753aff0a9303b76ee
Mohamed amine Tlili <foxweb585@gmail.com.com>
2023-10-24 18:13:39 +0100

    token reset

--- bin/Servers/authserver.dart
+++ bin/Servers/authserver.dart
@@ -19,44 +19,49 @@ class AuthServer {
 
   static DoJob() async {
     await init();
-    server.listen((HttpRequest authrequest) async {
-      Player? player;
-      authrequest.response.headers.contentType = ContentType.json;
-      authrequest.response.headers
-          .add(HttpHeaders.contentTypeHeader, "application/json");
-      Map<String, String> queryparm = authrequest.uri.queryParameters;
-      if (authrequest.uri.path == '/Signup/') {
-        player = await Authservice.getInstance().Signup(
-            queryparm.entries.first.value,
-            queryparm.entries.elementAt(1).value);
+    try {
+      server.listen((HttpRequest authrequest) async {
+        Player? player;
+        authrequest.response.headers.contentType = ContentType.json;
+        authrequest.response.headers
+            .add(HttpHeaders.contentTypeHeader, "application/json");
+        Map<String, String> queryparm = authrequest.uri.queryParameters;
+        if (authrequest.uri.path == '/Signup/') {
+          player = await Authservice.getInstance().Signup(
+              queryparm.entries.first.value,
+              queryparm.entries.elementAt(1).value);
 
-        if (player != null) {
-          authrequest.response
-              .write(json.encode({"message": "Player is signed up"}));
-        } else {
-          authrequest.response
-              .write(json.encode({"message": "Signing up failed"}));
+          if (player != null) {
+            authrequest.response
+                .write(json.encode({"message": "Player is signed up"}));
+          } else {
+            authrequest.response
+                .write(json.encode({"message": "Signing up failed"}));
+          }
+        } else if (authrequest.uri.path == '/Signin/') {
+          player = await Authservice.getInstance().Signin(
+              queryparm.entries.first.value,
+              queryparm.entries.elementAt(1).value);
+          if (player != null) {
+            String? token =
+                await Tokensservice.getInstance().prepare_token(player: player);
+            authrequest.response.write(json
+                .encode({"message": "Player is signed in", "token": token}));
+          } else {
+            authrequest.response
+                .write(json.encode({"message": "Signing in failed"}));
+          }
+        } else if (authrequest.uri.path == '/Delete/') {
+          final resp = await Authservice.getInstance().delete_user(
+              playername: queryparm.entries.first.value,
+              password: queryparm.entries.elementAt(1).value);
+          authrequest.response.write(json.encode(resp));
         }
-      } else if (authrequest.uri.path == '/Signin/') {
-        player = await Authservice.getInstance().Signin(
-            queryparm.entries.first.value,
-            queryparm.entries.elementAt(1).value);
-        if (player != null) {
-          String? token =
-              await Tokensservice.getInstance().prepare_token(player: player);
-          authrequest.response.write(
-              json.encode({"message": "Player is signed in", "token": token}));
-        } else {
-          authrequest.response
-              .write(json.encode({"message": "Signing in failed"}));
-        }
-      } else if (authrequest.uri.path == '/Delete/') {
-        final resp = await Authservice.getInstance().delete_user(
-            playername: queryparm.entries.first.value,
-            password: queryparm.entries.elementAt(1).value);
-        authrequest.response.write(json.encode(resp));
-      }
-      authrequest.response.close();
-    });
+        authrequest.response.close();
+      });
+    } catch (e) {
+      // should close gameserver
+      print("cannot start authserver !");
+    }
   }
 }
