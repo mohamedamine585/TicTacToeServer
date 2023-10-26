f2b3eb6d71421fe2114a2d2e7d5eb3f75c44af5a
Mohamed amine Tlili <foxweb585@gmail.com.com>
2023-10-26 22:20:52 +0100

    no own

--- test/server_test.dart
+++ test/server_test.dart
@@ -8,7 +8,7 @@ void main() {
   final port2 = '8081';
   final host2 = 'http://127.0.0.1:$port2';
 
-  String? prefix = "dssqd2547dds3d";
+  String? prefix = Random(45).toString();
   List<String> playernames = generateNames(10, prefix);
   List<String> passwords = generatePasswords(10);
   test_authandgameserver(playernames, passwords, host1, host2, 10);
