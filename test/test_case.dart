9f25129a7cdc49e43765cff44ad334c9cad99c7f
Mohamed amine Tlili <foxweb585@gmail.com.com>
2023-11-01 22:45:45 +0100

    big change

--- test/test_case.dart
+++ test/test_case.dart
@@ -138,6 +138,14 @@ test_update(List<String> playernames, List<String> passwords) {
         expect(jsonDecode(response.body)["message"],
             "playername changed to ${playernames[1]}K8M");
       });
+      test('First Ask To Play ', () async {
+        try {
+          player0 = await WebSocket.connect('ws://$HOST_GAME:$PORT_GAME',
+              headers: {"token": token0});
+        } catch (e) {
+          expect(e.toString().isNotEmpty, true);
+        }
+      });
       test('Update Password', () async {
         var response =
             await put(Uri.parse('http://$HOST_AUTH:$PORT_AUTH/ChangePassword/'),
@@ -148,14 +156,7 @@ test_update(List<String> playernames, List<String> passwords) {
                 }));
         expect(jsonDecode(response.body)["message"], "password changed");
       });
-      test('First Ask To Play ', () async {
-        try {
-          player0 = await WebSocket.connect('ws://$HOST_GAME:$PORT_GAME',
-              headers: {"token": token0});
-        } catch (e) {
-          expect(e.toString().isNotEmpty, true);
-        }
-      });
+
       test('Second Signin', () async {
         var response = await get(
           Uri.parse(
@@ -163,6 +164,14 @@ test_update(List<String> playernames, List<String> passwords) {
         );
         token0 = jsonDecode(response.body)["token"];
         expect(jsonDecode(response.body)["message"], "Player is signed in");
+      });
+      test('Second Ask To Play ', () async {
+        try {
+          player0 = await WebSocket.connect('ws://$HOST_GAME:$PORT_GAME',
+              headers: {"token": token0});
+        } catch (e) {
+          expect(e.toString().isNotEmpty, false);
+        }
       });
     });
   });
