e72bde0499160b5ec8f19f6367ed7226e6b0e53b
Mohamed amine Tlili <foxweb585@gmail.com.com>
2023-11-26 18:51:56 +0100

    0

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -2,6 +2,6 @@ const PORT_GAME = 8080;
 
 const PORT_AUTH = 8081;
 
-const HOST_GAME = '192.168.1.14';
+const HOST_GAME = '0.0.0.0';
 const HOST_AUTH = '0.0.0.0';
 const SECRET_SAUCE = "7@SecretTliliSauce@7";
