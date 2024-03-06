dd48c4f0e6627518aeced4a631505f43a727d13c
Mohamed amine Tlili <foxweb585@gmail.com.com>
2024-03-06 19:02:13 +0100

    fab++

--- test/test_case.dart
+++ test/test_case.dart
@@ -8,44 +8,29 @@ import 'package:web_socket_channel/io.dart';
 test_gameserver() {
   String message0 = "Room created";
   final env = DotEnv()..load();
-  group('************** Test ***************', () {
-    test("Connect to a play room1", () async {
-      final response = IOWebSocketChannel.connect(
-          Uri.parse("ws://localhost:${env["PORT"]}"),
-          headers: {
-            "Authorization":
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF5ZXJpZCI6IjY1ZDc3NjYwZTVlYjlmN2FmMDM5YmJmNCIsImlhdCI6MTcwODYzMzQyNiwiaXNzIjoiaHR0cHM6Ly9naXRodWIuY29tL2pvbmFzcm91c3NlbC9kYXJ0X2pzb253ZWJ0b2tlbiJ9.x12ElZDhNr_HIQBz5uJwNDrd4nRwBytkQ2lK1PifC8k",
-            "mode": "random"
-          });
-      response.stream.listen((event) {
-        expect(json.decode(event)["message"], message0);
-      });
-    });
-    test("Connect to a play room2", () async {
-      final response = IOWebSocketChannel.connect(
-          Uri.parse("ws://localhost:${env["PORT"]}"),
-          headers: {
-            "Authorization":
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF5ZXJpZCI6IjY1ZTcyNTY3NDAzYTNlNTkxYTMwMWExNSIsImlhdCI6MTcwOTY0NzIwNywiaXNzIjoiaHR0cHM6Ly9naXRodWIuY29tL2pvbmFzcm91c3NlbC9kYXJ0X2pzb253ZWJ0b2tlbiJ9.K4fV-ReDKIomtTGFQ6-X4766rry4batvLHYPaiVzNA8"
-          });
 
-      response.stream.listen((event) async {
-        message0 = "Opponent found !";
+  test("Connect to a play room2", () async {
+    final response = IOWebSocketChannel.connect(
+        Uri.parse("ws://localhost:${env["PORT"]}"),
+        headers: {
+          "Authorization":
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF5ZXJpZCI6IjY1ZTcyNTY3NDAzYTNlNTkxYTMwMWExNSIsImlhdCI6MTcwOTY0NzIwNywiaXNzIjoiaHR0cHM6Ly9naXRodWIuY29tL2pvbmFzcm91c3NlbC9kYXJ0X2pzb253ZWJ0b2tlbiJ9.K4fV-ReDKIomtTGFQ6-X4766rry4batvLHYPaiVzNA8"
+        });
 
-        expect(json.decode(event)["message"], "Opponent found !");
-        if (json.decode(event)["message"] == "Opponent found !") {
-          await response.sink.close();
-        }
-      });
+    response.stream.listen((event) async {
+      expect(json.decode(event)["message"], message0);
+      if (json.decode(event)["message"] == "Opponent found !") {
+        await response.sink.close();
+      }
     });
-    test("get doc", () async {
-      final response =
-          await get(Uri.parse("http://localhost:8080/player"), headers: {
-        "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF5ZXJpZCI6IjY1ZDc3NjYwZTVlYjlmN2FmMDM5YmJmNCIsImlhdCI6MTcwODYzMzQyNiwiaXNzIjoiaHR0cHM6Ly9naXRodWIuY29tL2pvbmFzcm91c3NlbC9kYXJ0X2pzb253ZWJ0b2tlbiJ9.x12ElZDhNr_HIQBz5uJwNDrd4nRwBytkQ2lK1PifC8k"
-      });
-      expect(response.statusCode, 200);
-      expect(json.decode(response.body)["email"], isNotNull);
+  });
+  test("get doc", () async {
+    final response =
+        await get(Uri.parse("http://localhost:8080/player"), headers: {
+      "Authorization":
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF5ZXJpZCI6IjY1ZDc3NjYwZTVlYjlmN2FmMDM5YmJmNCIsImlhdCI6MTcwODYzMzQyNiwiaXNzIjoiaHR0cHM6Ly9naXRodWIuY29tL2pvbmFzcm91c3NlbC9kYXJ0X2pzb253ZWJ0b2tlbiJ9.x12ElZDhNr_HIQBz5uJwNDrd4nRwBytkQ2lK1PifC8k"
     });
+    expect(response.statusCode, 200);
+    expect(json.decode(response.body)["email"], isNotNull);
   });
 }
